8e7c8e0688d895bdd1c1e92331db280c3bbcd29f
Rajat Kishore Acharya <rajat.getmeonfb@gmail.com>
2024-02-19 05:30:34 +0530

    Created basic structure

---
+++ pdfviewer.py
@@ -0,0 +1,19 @@
+from tkinter import *   # importing everything from tkinter
+from tkinter import ttk # import ttk module for styling widgets
+from tkinter import filedialog as fd
+import os
+
+
+# PDF viewer class
+class PDFViewer:
+    def __init__(self, master):
+        pass
+
+root = Tk()
+
+app = PDFViewer(root)
+
+root.mainloop()
+
+
+
